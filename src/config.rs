91a403b8ea845a62c59dc85e22d8db54b6822c2f
Китаев Роман Викторович <kitaev@tochka.com>
2026-08-13 01:45:19 +0300

    refactor: collapse config newtypes into a macro

--- src/config.rs
+++ src/config.rs
@@ -1,168 +1,64 @@
 use std::collections::HashMap;
-use std::fmt;
-use std::ops::Deref;
 
-/// Newtype for attribute prefix (e.g., "@" for "@id", "@class")
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct AttrPrefix(String);
+macro_rules! config_newtype {
+    ($(#[$doc:meta])* $name:ident, $default:literal) => {
+        $(#[$doc])*
+        #[derive(Clone, Debug, PartialEq, Eq)]
+        pub struct $name(String);
 
-impl AttrPrefix {
-    pub fn new(s: impl Into<String>) -> Self {
-        Self(s.into())
-    }
+        impl $name {
+            pub fn new(s: impl Into<String>) -> Self {
+                Self(s.into())
+            }
+        }
+
+        impl Default for $name {
+            fn default() -> Self {
+                Self($default.to_owned())
+            }
+        }
+
+        impl std::ops::Deref for $name {
+            type Target = str;
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        impl AsRef<str> for $name {
+            fn as_ref(&self) -> &str {
+                &self.0
+            }
+        }
+
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+    };
 }
 
-impl Default for AttrPrefix {
-    fn default() -> Self {
-        Self("@".to_owned())
-    }
-}
-
-impl Deref for AttrPrefix {
-    type Target = str;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl AsRef<str> for AttrPrefix {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
-}
-
-impl fmt::Display for AttrPrefix {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-/// Newtype for CDATA key (e.g., "#text")
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct CdataKey(String);
-
-impl CdataKey {
-    pub fn new(s: impl Into<String>) -> Self {
-        Self(s.into())
-    }
-}
-
-impl Default for CdataKey {
-    fn default() -> Self {
-        Self("#text".to_owned())
-    }
-}
-
-impl Deref for CdataKey {
-    type Target = str;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl AsRef<str> for CdataKey {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
-}
-
-impl fmt::Display for CdataKey {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl PartialEq<str> for CdataKey {
-    fn eq(&self, other: &str) -> bool {
-        self.0 == other
-    }
-}
-
-impl PartialEq<CdataKey> for str {
-    fn eq(&self, other: &CdataKey) -> bool {
-        self == other.0
-    }
-}
-
-impl PartialEq<String> for CdataKey {
-    fn eq(&self, other: &String) -> bool {
-        self.0 == *other
-    }
-}
+config_newtype!(
+    /// Attribute prefix (e.g., "@" for "@id", "@class")
+    AttrPrefix, "@"
+);
+config_newtype!(
+    /// Key for text content (e.g., "#text")
+    CdataKey, "#text"
+);
+config_newtype!(
+    /// Key for comment content (e.g., "#comment")
+    CommentKey, "#comment"
+);
+config_newtype!(
+    /// Separator between namespace and local name (e.g., ":")
+    NamespaceSeparator, ":"
+);
 
 impl PartialEq<CdataKey> for String {
     fn eq(&self, other: &CdataKey) -> bool {
         *self == other.0
-    }
-}
-
-/// Newtype for comment key (e.g., "#comment")
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct CommentKey(String);
-
-impl CommentKey {
-    pub fn new(s: impl Into<String>) -> Self {
-        Self(s.into())
-    }
-}
-
-impl Default for CommentKey {
-    fn default() -> Self {
-        Self("#comment".to_owned())
-    }
-}
-
-impl Deref for CommentKey {
-    type Target = str;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl AsRef<str> for CommentKey {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
-}
-
-impl fmt::Display for CommentKey {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-/// Newtype for namespace separator (e.g., ":")
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct NamespaceSeparator(String);
-
-impl NamespaceSeparator {
-    pub fn new(s: impl Into<String>) -> Self {
-        Self(s.into())
-    }
-}
-
-impl Default for NamespaceSeparator {
-    fn default() -> Self {
-        Self(":".to_owned())
-    }
-}
-
-impl Deref for NamespaceSeparator {
-    type Target = str;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl AsRef<str> for NamespaceSeparator {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
-}
-
-impl fmt::Display for NamespaceSeparator {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
     }
 }
 
